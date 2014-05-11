d502af7e43281e902f3da9c51312e25b47703eaf
Thien Truong <moments1ever+pub@gmail.com>
2014-05-11 08:05:48 -0500

    Clean up find_multiples_of_3_and_5 method

--- 1/lib/multiple_finder.rb
+++ 1/lib/multiple_finder.rb
@@ -1,17 +1,6 @@
 class MultipleFinder
   def find_multiples_of_3_and_5(max_multiple)
-    multiples = []
-    multiple_of_3 = 3
-    multiple_of_5 = 5
-    while multiple_of_3 < max_multiple
-      multiples.push(multiple_of_3)
-      multiple_of_3 = multiple_of_3 + 3
-    end
-    while multiple_of_5 < max_multiple
-      multiples.push(multiple_of_5)
-      multiple_of_5 = multiple_of_5 + 5
-    end
-    multiples.sort.uniq
+    (1...max_multiple).find_all { |i| i % 3 == 0 || i % 5 == 0 }
   end
 
   def sum_multiples_of_3_and_5(max_multiple)
