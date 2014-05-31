f56a58378b470b460710b94b2a50c7133bac65d5
Thien Truong <moments1ever+pub@gmail.com>
2014-05-31 02:44:49 -0500

    Find largest palindrome product.

--- 4/lib/largest_palindrome_product.rb
+++ 4/lib/largest_palindrome_product.rb
@@ -2,4 +2,16 @@ class LargestPalindromeProduct
   def generate_products(_begin, _end)
     (_begin.._end).to_a.combination(2).map { |pair| pair[0] * pair[1] }
   end
+
+  def find_largest_palindrome_product(_begin, _end)
+    products = generate_products(_begin, _end)
+    largest_palindrome_product = 0
+    products.reverse_each do |product|
+      if product.to_s == product.to_s.reverse
+        largest_palindrome_product = product
+        break
+      end
+    end
+    largest_palindrome_product
+  end
 end
--- 4/test/largest_palindrome_product_spec.rb
+++ 4/test/largest_palindrome_product_spec.rb
@@ -5,4 +5,9 @@ describe LargestPalindromeProduct do
     largest_palindrome_product = LargestPalindromeProduct.new
     largest_palindrome_product.generate_products(1,4).should eq([2,3,4,6,8,12])
   end
+
+  it 'find the largest palindrome product' do
+    largest_palindrome_product = LargestPalindromeProduct.new
+    largest_palindrome_product.find_largest_palindrome_product(1,11).should eq(99)
+  end
 end
