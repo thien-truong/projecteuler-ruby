ab9b022b1dc41a165c97c430af9a20479154b37d
Thien Truong <moments1ever+pub@gmail.com>
2014-06-03 20:46:22 -0500

    Reformat and refactor.

--- 6/lib/sum_square_difference.rb
+++ 6/lib/sum_square_difference.rb
@@ -1,10 +1,10 @@
 class SumSquareDifference
-  def sum_squares(_begin,_end)
-    (_begin.._end).to_a.map { |number | number * number}.reduce(:+)
+  def sum_squares(_begin, _end)
+    (_begin.._end).to_a.map { |number | number * number }.reduce(:+)
   end
 
-  def square_sum(_begin,_end)
-    (_begin.._end).to_a.reduce(:+).**2
+  def square_sum(_begin, _end)
+    (_begin.._end).to_a.reduce(:+)**2
   end
 
   def find_sum_square_difference(_begin, _end)
--- 6/test/sum_square_difference_spec.rb
+++ 6/test/sum_square_difference_spec.rb
@@ -1,19 +1,20 @@
 require_relative '../lib/sum_square_difference'
 
 describe SumSquareDifference do
+  before(:each) do
+    @sum_square_difference = SumSquareDifference.new
+  end
+
  it 'sums the squares of the first ten natural number' do
-   sum_square_difference = SumSquareDifference.new
-   sum_square_difference.sum_squares(1, 10).should == 385
+   @sum_square_difference.sum_squares(1, 10).should == 385
  end
 
   it 'squares the sum of the first ten natural number' do
-    sum_square_difference = SumSquareDifference.new
-    sum_square_difference.square_sum(1, 10).should == 3025
+    @sum_square_difference.square_sum(1, 10).should == 3025
   end
 
   it 'find difference between the sum of the squares of
 the first ten natural numbers and its square of the sum' do
-    sum_square_difference = SumSquareDifference.new
-    sum_square_difference.find_sum_square_difference(1, 10).should == 2640
+    @sum_square_difference.find_sum_square_difference(1, 10).should == 2640
   end
 end
