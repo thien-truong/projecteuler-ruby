871a0056399a416b4d4c1a5885600ff4159ba16f
Thien Truong <moments1ever+pub@gmail.com>
2014-06-10 16:31:44 -0500

    Refactor and find largest product.

--- 8/lib/largest_product_in_series.rb
+++ 8/lib/largest_product_in_series.rb
@@ -1,9 +1,19 @@
 require_relative 'series_of_numbers'
 
 class LargestProductInSeries
-  def find_product(number_of_digits)
-    series_of_numbers = SeriesOfNumbers::SERIES_OF_NUMBER.to_s.split(//)
-    series_of_numbers.map! { |number| number.to_i }
-    series_of_numbers[0, number_of_digits].reduce(1,:*)
+
+  def prepare_series
+    SeriesOfNumbers::SERIES_OF_NUMBER.to_s.split(//).map! { |number| number.to_i }
+  end
+
+  def find_largest_product(number_of_digits)
+    series_of_number = prepare_series
+    starting_index = 0
+    products = []
+    until starting_index == (series_of_number.length - number_of_digits + 1)
+      products << series_of_number[starting_index, number_of_digits].reduce(1, :*)
+      starting_index = starting_index + 1
+    end
+    products.max
   end
 end
--- 8/test/largest_product_in_a_series_spec.rb
+++ 8/test/largest_product_in_a_series_spec.rb
@@ -2,10 +2,10 @@ require_relative '../lib/largest_product_in_series'
 
 describe LargestProductInSeries do
   before(:each) do
-  @largest_product_in_series = LargestProductInSeries.new
+    @largest_product_in_series = LargestProductInSeries.new
   end
 
-  it 'calculate the product of the first 4 numbers in the series' do
-    expect @largest_product_in_series.find_product(4).should == 126
+  it 'finds the largest product of 4 adjacent digits in a series' do
+    expect @largest_product_in_series.find_largest_product(4).should == 5832
   end
 end
