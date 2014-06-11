f5f60cd835f0d5f45f654bf093fd0262bd90d09a
Thien Truong <moments1ever+pub@gmail.com>
2014-06-10 19:45:23 -0500

    Refactor.

--- 8/lib/largest_product_in_series.rb
+++ 8/lib/largest_product_in_series.rb
@@ -1,19 +1,14 @@
 require_relative 'series_of_numbers'
 
 class LargestProductInSeries
-
   def prepare_series
     SeriesOfNumbers::SERIES_OF_NUMBER.to_s.split(//).map! { |number| number.to_i }
   end
 
   def find_largest_product(number_of_digits)
     series_of_number = prepare_series
-    starting_index = 0
     products = []
-    until starting_index == (series_of_number.length - number_of_digits + 1)
-      products << series_of_number[starting_index, number_of_digits].reduce(1, :*)
-      starting_index = starting_index + 1
-    end
+    series_of_number.each_cons(number_of_digits) { |numbers| products << numbers.reduce(1, :*) }
     products.max
   end
 end
